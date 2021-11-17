b6532512d4643c2efea9b4c165a8280171501213
Raúl Jiménez <rjsanche@gmail.com>
2021-11-17 22:47:33 +0100

    Add project files.

---
+++ DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessorTests.cs
@@ -0,0 +1,33 @@
+﻿using DeskBooker.Core.Domain;
+using System;
+using Xunit;
+
+namespace DeskBooker.Core.Processor
+{
+    public class DeskBookingRequestProcessorTests
+    {
+        [Fact]
+        public void ShouldReturnDeskBookingResultWithRequestValues()
+        {
+            //arrange
+            var request = new DeskBookingRequest
+            {
+                FirstName = "Raúl",
+                LastName = "Jiménez",
+                Email = "rjsanche@gmail.com",
+                Date = new DateTime(2021, 11, 17),
+            };
+            var processor = new DeskBookingRequestProcessor();
+
+            //act
+            DeskBookingResult result = processor.BookDesk(request);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(request.FirstName, result.FirstName);
+            Assert.Equal(request.LastName, result.LastName);
+            Assert.Equal(request.Email, result.Email);
+            Assert.Equal(request.Date, result.Date);
+        }
+    }
+}
